b5a83d50566aaea77e01be5ca0e6c60e3c33a941
Iosif Furtuna <furtuna.iosif@yahoo.com>
2021-08-04 14:58:32 +0300

    deleted js files

--- app/oidc-test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const openid_client_1 = require("openid-client");
-openid_client_1.Issuer.discover('https://oidc.i3m.gold.upc.edu/oidc').then(function (issuer) {
-    console.log('Discovered issuer %s %O', issuer.issuer, issuer.metadata);
-}).catch(err => { throw new Error(err); });
